f86d83fdad461856cd08451d70d6949898f124f7
Marilou <6110@holbertonstudents.com>
2023-01-25 11:43:14 -0800

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-/* main - Entry point */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf(
-
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
