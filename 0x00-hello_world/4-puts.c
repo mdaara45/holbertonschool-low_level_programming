62ca289114975d9d5b95d0ab9a755080e67746cc
Marilou <6110@holbertonstudents.com>
2023-01-25 11:08:06 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-/* main - Entry point */
+
+
+/**
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a new line."
+ * return 0.
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* main - Entry point */
+int main(void)
+{
+printf(
+
+}
