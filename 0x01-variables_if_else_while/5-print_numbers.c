fceed301044e61dbaaf515a55a123b5253e838c2
Marilou <6110@holbertonstudents.com>
2023-01-26 19:21:40 -0800

    number

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,11 +9,11 @@
  */
 int main(void)
 {
-int x = 0;
-for (x = 0; x <= 9; x++)
+char x = '0';
+for (x = '0'; x <= '9'; x++)
 {
-printf("%d", x);
-printf("\n");
+putchar(x);
+putchar('\n');
 }
 return (0);
 }
