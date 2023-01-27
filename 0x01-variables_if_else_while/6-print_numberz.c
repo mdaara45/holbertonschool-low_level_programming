baa76c321bd47636e5592ecd4684787c59a6b6ba
Marilou <6110@holbertonstudents.com>
2023-01-27 09:04:37 -0800

    corrections

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,10 +9,13 @@
  */
 int main(void)
 {
-for (int i = '0'; i <= '9'; i++)
-{
-putchar(i);
-}
-putchar('\n');
-return (0);
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
 }
