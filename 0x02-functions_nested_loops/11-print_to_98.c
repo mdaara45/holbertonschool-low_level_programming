ca0f552853d0d0764c03eecee0c5f6fee557ea90
Marilou <6110@holbertonstudents.com>
2023-01-29 16:27:29 -0800

    fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,20 +7,9 @@
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	for (; n != 98; n += (n > 98) ? -1 : 1)
 	{
-		for (n = n; n < 98; n++)
-		{
-			printf("%d, ", n);
-			printf("%d\n", 98);
-		}
+		printf("%d, ", n);
 	}
-	else
-	{
-		for (n = n; n > 98; n--)
-		{
-			printf("%d, ", n);
-			printf("%d\n", 98);
-		}
-	}
+	printf("98\n");
 }
