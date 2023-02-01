782f267d5e619fa994970d82da248e022429859e
Marilou <6110@holbertonstudents.com>
2023-02-01 13:32:32 -0800

    print numbers up to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers 0 to 14.
+ *
+ */
+void more_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 15; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
