02f49be287ced224cf93403413b2bab008bb0efb
Marilou <6110@holbertonstudents.com>
2023-02-01 13:41:05 -0800

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,11 +6,16 @@
  */
 void more_numbers(void)
 {
-	int x;
+	int x, y;
 
-	for (x = 0; x < 15; x++)
+	for (x = 0; x <= 9; x++)
 	{
-		_putchar((x % 10) + '0');
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
